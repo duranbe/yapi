dcc126eb65d947377c1960300329d446553d4836
Benoit Durand <benoit.durand.mail@gmail.com>
2024-07-27 20:31:39 +0200

    fx: refactor test

--- tst/setup.py
+++ tst/setup.py
@@ -1,6 +1,15 @@
 import unittest
+import logging
+import re
 
 
 class YapiTestCase(unittest.TestCase):
     def setUp(self) -> None:
-        print("\n", unittest.TestCase.id(self))
+        logging.basicConfig(level=logging.INFO)
+        logging.getLogger().setLevel(logging.INFO)
+        logging.getLogger().handlers[0].setFormatter(
+            logging.Formatter("\n%(levelname)s:%(message)s")
+        )
+        current_test = unittest.TestCase.id(self)
+        test_name = re.sub(r"^.*?\.", "", current_test)
+        logging.info(test_name)
--- tst/test_server.py
+++ tst/test_server.py
@@ -4,23 +4,17 @@ from subprocess import Popen
 import time
 import logging
 
-SERVER_START_WAIT_TIME = 1
+SERVER_START_WAIT_TIME = 0.1
 
 
 class TestServer(YapiTestCase):
     def setUp(self) -> None:
         self.process = Popen(["python3", "-m", "tst.sample_server"])
         time.sleep(SERVER_START_WAIT_TIME)
-        logging.basicConfig(level=logging.DEBUG)
-        logging.getLogger().setLevel(logging.DEBUG)
-        logging.getLogger().handlers[0].setFormatter(
-            logging.Formatter("%(asctime)s:%(levelname)s:%(message)s")
-        )
-        logging.debug("Sample Server started with pid : %d", self.process.pid)
+        logging.info("Sample Server started with pid : %d", self.process.pid)
 
     def test_simple_connectivity_check(self):
         url = "http://localhost:4221"
-
         with urlopen(url) as response:
             self.assertEqual(response.getcode(), 200)
             self.assertEqual(response.read().decode(), "test")
@@ -28,4 +22,4 @@ class TestServer(YapiTestCase):
     def tearDown(self):
         self.process.kill()
         self.process.wait()
-        logging.debug("Sample Server with pid terminated : %d", self.process.pid)
+        logging.info("Sample Server with pid %d has been terminated", self.process.pid)
