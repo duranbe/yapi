13c186b89002c6d450620b2d4682f44d413238e5
Benoit Durand <benoit.durand.mail@gmail.com>
2024-07-27 14:56:17 +0200

    fx: fix functional test

--- tst/sample_server.py
+++ tst/sample_server.py
@@ -4,10 +4,10 @@ from src.server.server import Server
 
 
 def process(sock, addr):
-    response = Response(status=HTTP_200, headers={}, body=b"test")._as_bytes()
+    response = Response(status=HTTP_200, headers={}, body="test")._as_bytes()
     sock.send(response)
     sock.close()
 
 
 if __name__ == "__main__":
-    Server("127.0.0.1", 4221, process=process)
+    Server("localhost", 4221, process=process)
--- tst/test_server.py
+++ tst/test_server.py
@@ -1,18 +1,30 @@
 from tst.setup import YapiTestCase
 from urllib.request import urlopen
 from subprocess import Popen
+import os
+import time
+import logging
 
+SERVER_START_WAIT_TIME = 1
 
 class TestServer(YapiTestCase):
     def setUp(self) -> None:
         self.process = Popen(["python3", "-m", "tst.sample_server"])
-
+        time.sleep(SERVER_START_WAIT_TIME)
+        logging.basicConfig(level=logging.DEBUG)
+        logging.getLogger().setLevel(logging.DEBUG)
+        logging.getLogger().handlers[0].setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(message)s'))
+        logging.debug("Sample Server started with pid : %d",self.process.pid)
+       
     def test_simple_connectivity_check(self):
-        url = "http://127.0.0.1:4221"
+        url = "http://localhost:4221"
 
         with urlopen(url) as response:
             self.assertEqual(response.getcode(), 200)
+        
 
     def tearDown(self):
+
         self.process.kill()
         self.process.wait()
+        logging.debug("Sample Server with pid terminated : %d",self.process.pid)
