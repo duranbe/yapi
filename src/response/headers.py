7841910bfded1f559c4b3f22dba3efafd4fbe6e6
Benoit Durand <benoit.durand.mail@gmail.com>
2024-07-31 20:37:45 +0200

    ft: add test and prepare method specification

--- src/response/headers.py
+++ src/response/headers.py
@@ -1,2 +1,3 @@
 CONTENT_LENGTH = "Content-Length"
 CONTENT_TYPE = "Content-Type"
+ALLOW = "Allow"
--- tst/test_request.py
+++ tst/test_request.py
@@ -13,3 +13,15 @@ class TestRequest(YapiTestCase):
         self.assertEqual(request.protocol, "HTTP/1.1")
         self.assertEqual(request.headers, {"Accept-Encoding": "gzip"})
         self.assertEqual(request.body, "")
+
+
+    def test_request_parse_headers_and_url(self):
+            request_data = b"POST /test/test/test HTTP/1.1\r\nAccept-Encoding: gzip\r\n\r\nbody"
+
+            request = Request(request_data)
+
+            self.assertEqual(request.method, "POST")
+            self.assertEqual(request.endpoint, "/test/test/test")
+            self.assertEqual(request.protocol, "HTTP/1.1")
+            self.assertEqual(request.headers, {"Accept-Encoding": "gzip"})
+            self.assertEqual(request.body, "body")
