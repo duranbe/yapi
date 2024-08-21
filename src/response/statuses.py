2efb47e46052f93238ce5e066903bc41b334b6be
Benoit Durand <benoit.durand.mail@gmail.com>
2024-08-21 22:56:19 +0200

    ft: add more reponse statuses

--- src/response/statuses.py
+++ src/response/statuses.py
@@ -1,10 +1,15 @@
 # 2xx - OK
 HTTP_200 = "200 OK"
 HTTP_201 = "201 Created"
+HTTP_202 = "202 Accepted"
 
 
 # 4xx - Client Side Error
 HTTP_400 = "400 Bad Request"
 HTTP_401 = "401 Unauthorized"
+HTTP_403 = "403 Forbidden"
 HTTP_404 = "404 Not Found"
 HTTP_405 = "405 Method Not Allowed"
+
+# 5xx - Server Side Error
+HTTP_500 = "Internal Server Error"
